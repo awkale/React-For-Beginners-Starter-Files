d36639d6f1ed4a7d5006beca76f732ae3b5b5b0b
Alex Kale <hi@awkale.me>
2018-10-06 20:23:38 -0400

    add fish form and state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,20 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1 take copy of existing state
+    const fishes = {...this.state.fishes};
+    // 2 add new fish to fishes
+    fishes[`fish${Date.now()}`] = fish;
+    // 3 set new fishes object to state
+    this.setState({ fishes }); // updating using es6 prop and value same name
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +25,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"></Header>
         </div>
         <Order></Order>
-        <Inventory></Inventory>
+        <Inventory addFish={this.addFish}></Inventory>
       </div>
     )
   }
