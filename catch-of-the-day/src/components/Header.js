bb81ea8934cf6e89629d299b735b0293ac9e4a08
Alex Kale <hi@awkale.me>
2018-10-07 19:56:57 -0400

    add proptypes

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Header = ({tagline}) => (
+const Header = props => (
   <header className="top">
-    <h1>Catch
+    <h1>
+      Catch
       <span className="ofThe">
-        <span className="of">
-          of
-        </span>
-        <span className="the">
-          the
-        </span>
+        <span className="of">Of</span>
+        <span className="the">The</span>
       </span>
       Day
     </h1>
     <h3 className="tagline">
-      <span>{tagline}</span>
+      <span>{props.tagline}</span>
     </h3>
   </header>
 );
 
-export default Header
+Header.propTypes = {
+  tagline: PropTypes.string.isRequired
+};
+
+export default Header;
